58fa1bf4b3d417488b606ddaae5807a02121c19e
ewertonmontes <120966006+ewertonmontes@users.noreply.github.com>
2023-01-14 13:15:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# academia010
+ Projeto academia
---
+++ scripts/scripts.js
@@ -0,0 +1,92 @@
+/* === Ativar borda inferior na rolagem do menu === */
+    const menuAtivo = document.querySelector('header');
+
+    function activeScroll() {
+        menuAtivo.classList.toggle('ativo', scrollY > 0);
+    }
+
+    window.addEventListener('scroll', activeScroll);
+/* === Fim === */
+
+
+/* === Expandir menu hambúrguer no clique === */
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".menu-alternativo",
+  ".menu1",
+);
+mobileNavbar.init();
+/* === Fim === */
+
+
+/* === Animações durante a rolagem === */
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+      const context = this;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+  
+  const target = document.querySelectorAll('[data-anime]');
+  const animationClass = 'animate';
+  
+  function animeScroll() {
+    const windowTop = window.pageYOffset + (window.innerHeight * 0.75);
+    target.forEach(function(element) {
+      if((windowTop) > element.offsetTop) {
+        element.classList.add(animationClass);
+      } else {
+        element.classList.remove(animationClass);
+      }
+    })
+  }
+  
+  animeScroll();
+  
+  if(target.length) {
+    window.addEventListener('scroll', debounce(function() {
+      animeScroll();
+    }, 10));
+  }
+/* === Fim === */
+
+
+/* === Alterar ícone do WhatsApp === */
+function mudalogo1(logo1) {
+    document.getElementById('whatsapp1').src = logo1;
+}
+/* === Fim === */
